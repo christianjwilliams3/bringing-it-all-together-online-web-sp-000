9cd2b860347c7c0614650ff8bc567354d7437b0d
christian williams <christian.williamsj@gmail.com>
2019-12-19 19:03:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -85,4 +85,18 @@ def self.find_or_create_by(name:, breed:)
    end
    dog
  end
+
+ def self.find_by_name(name)
+   sql = <<-SQL
+   SELECT *
+   FROM dogs
+   WHERE name = ?
+   LIMIT 1 
+   SQL
+   DB[:conn].execute(sql, name).map do |row|
+     self.new_from_db(row)
+   end.first
+ end
+   
+ 
 end
